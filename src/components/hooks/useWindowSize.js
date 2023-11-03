b7cfb12835cd88e7bb80cef9fa4989915eda218d
VicMoraGit <victor.mora.4b@gmail.com>
2023-11-03 19:36:22 +0100

    feat: ✨ window size hook

---
+++ src/components/hooks/useWindowSize.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+const getWindowSize = () => {
+  const { innerHeight: height, innerWidth: width } = window;
+
+  return {
+    height,
+    width,
+  };
+};
+
+const useWindowSize = () => {
+  const [size, setSize] = useState(getWindowSize());
+
+  useEffect(() => {
+    const handleResize = () => setSize(getWindowSize());
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return size;
+};
+
+export default useWindowSize;
